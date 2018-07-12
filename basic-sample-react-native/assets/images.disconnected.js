2ba160e5c9290560d2378dc1859fda2a7ad5c054
Chad Carpenter <chad.carpenter@arke.com>
2018-07-12 13:38:40 +0200

    Slightly fleshed out AR scene
    Added image, lights, and 3d model to sample AR scene. Had to add VRX extension support in order to do so.

--- basic-sample-react-native/assets/images.disconnected.js
+++ basic-sample-react-native/assets/images.disconnected.js
@@ -5,4 +5,17 @@ const images = {
   '/data/media/img/sc_logo.png': require('../data/media/img/sc_logo.png'),
 };
 
-export { images };
+const models = {
+  // Soccer ball
+  '/data/media/model/object_soccerball/object_soccer_ball_diffuse.png': require('../data/media/model/object_soccerball/object_soccer_ball_diffuse.png'),
+  '/data/media/model/object_soccerball/object_soccer_ball_normal.png': require('../data/media/model/object_soccerball/object_soccer_ball_normal.png'),
+  '/data/media/model/object_soccerball/object_soccer_ball_specular.png': require('../data/media/model/object_soccerball/object_soccer_ball_specular.png'),
+  '/data/media/model/object_soccerball/object_soccer_ball.vrx': require('../data/media/model/object_soccerball/object_soccer_ball.vrx'),
+  // Emoji smile
+  '/data/media/model/emoji_smile/emoji_smile_diffuse.png': require('../data/media/model/emoji_smile/emoji_smile_diffuse.png'),
+  '/data/media/model/emoji_smile/emoji_smile_normal.png': require('../data/media/model/emoji_smile/emoji_smile_normal.png'),
+  '/data/media/model/emoji_smile/emoji_smile_specular.png': require('../data/media/model/emoji_smile/emoji_smile_specular.png'),
+  '/data/media/model/emoji_smile/emoji_smile.vrx': require('../data/media/model/emoji_smile/emoji_smile.vrx'),
+};
+
+export { images, models };
--- basic-sample-react-native/src/App.js
+++ basic-sample-react-native/src/App.js
@@ -4,13 +4,19 @@ import { Placeholder, SitecoreContext } from '@sitecore-jss/sitecore-jss-react-n
 // eslint-disable-next-line
 import { getRouteData } from 'data-service';
 import componentFactory from './componentFactory';
+import { images, models } from 'static-assets';
 
 import {
   ViroVRSceneNavigator,
   ViroARSceneNavigator,
   ViroScene,
   ViroARScene,
-  ViroText
+  ViroAmbientLight,
+  ViroText,
+  ViroImage,
+  ViroNode,
+  ViroSpotLight,
+  Viro3DObject
 } from 'react-viro';
 
 // See instructions in ./cofig.example.js for setting up Viro API key
@@ -66,7 +72,38 @@ class App extends React.Component {
             scene: () => {
               return (
                 <ViroARScene>
+                  <ViroAmbientLight color={"#aaaaaa"} influenceBitMask={1} />
                   <ViroText text='React Native + JSS + Viro = Sitecore AR' scale={[.5, .5, .5]} position={[0, 0, -1]} width={2} transformBehaviors={["billboardY"]} />
+                  <ViroImage source={images['/assets/img/banner.jpg']} width={1} height={.2} position={[-2, .5, -1]} />
+                  
+                  <ViroNode position={[.5, -.5, -.5]} dragType="FixedToWorld" onDrag={() => { }} >
+                    <ViroSpotLight
+                      innerAngle={5}
+                      outerAngle={45}
+                      direction={[0, -1, -.2]}
+                      position={[0, 3, 0]}
+                      color="#ffffff"
+                      castsShadow={true}
+                      influenceBitMask={4}
+                      shadowMapSize={2048}
+                      shadowNearZ={2}
+                      shadowFarZ={5}
+                      shadowOpacity={.7} />
+
+                    <Viro3DObject
+                      source={models['/data/media/model/emoji_smile/emoji_smile.vrx']}
+                      position={[0, .15, 0]}
+                      scale={[.3, .3, .3]}
+                      type="VRX"
+                      lightReceivingBitMask={5}
+                      shadowCastingBitMask={4}
+                      resources={[
+                        models['/data/media/model/emoji_smile/emoji_smile_diffuse.png'],
+                        models['/data/media/model/emoji_smile/emoji_smile_normal.png'],
+                        models['/data/media/model/emoji_smile/emoji_smile_specular.png']
+                      ]} />
+                  </ViroNode>
+
                 </ViroARScene>
               );
             }
